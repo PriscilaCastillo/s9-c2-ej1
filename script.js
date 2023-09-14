0235405846c10d8c880e674a1140d0b635a3f8e2
Priscila Castillo <pcastillof97@gmail.com>
2023-09-13 21:39:22 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let numeros = [10, 20, 30, 40, 50];
+let suma=0;
+
+for (let i = 0; i < numeros.length; i++) {
+    suma = suma + numeros[i];
+  }
+
+  console.log(suma);
+
+let promedio = suma/numeros.length;
+console.log(promedio);
